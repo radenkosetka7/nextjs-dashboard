ba6c3f8e8f63e4829ae2380f3bd8436cdc7e628f
radenko.setka <radenkosetka7@gmail.com>
2025-09-27 14:13:51 +0200

    authentication impelemnted

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const CustomersPage = () => {
-  return (
-    <div>CustomersPage</div>
-  )
-}
+  return <div>CustomersPage</div>;
+};
 
-export default CustomersPage
+export default CustomersPage;
--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DashoboardPage = () => {
-  return (
-    <div>DashoboardPage</div>
-  )
-}
-
-export default DashoboardPage
